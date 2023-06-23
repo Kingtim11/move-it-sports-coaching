9c7197a8289358a1d7728f494c7232d93baf7124
Timothy King <king_tim@hotmail.co.uk>
2023-06-23 17:35:01 +0100

    Routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import './App.css';
-import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/HomePage';
 import Navbar from '../src/components/Navbar';
 import BookingPage from '../src/pages/BookingPage';
 import WhatWeOfferPage from './pages/WhatWeOfferPage';
 import MeetTheTeamPage from './pages/MeetTheTeamPage';
 import ScrollToTop from './components/ScrollToTop';
-import RootLayout from './layouts/RootLayout';
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-          <Route path="/" element={<RootLayout />}>
-            <Route index element={<Home />} />
-            <Route path="/what-we-offer" element={<WhatWeOfferPage />} />
-            <Route path="/meet-the-team" element={<MeetTheTeamPage />} />
-            <Route path="/bookings" element={<BookingPage />} />
-          </Route>
-  )
-)
 
 export default function App() {
   return (
-    <RouterProvider router={router} />
+    <div className="App">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />}>
+          <Route path="/what-we-offer" element={<WhatWeOfferPage />} />
+          <Route path="/meet-the-team" element={<MeetTheTeamPage />} />
+          <Route path="/bookings" element={<BookingPage />} />
+          </Route>
+        </Routes>
+        <Navbar />
+      </BrowserRouter>
+    </div>
   );
 }
