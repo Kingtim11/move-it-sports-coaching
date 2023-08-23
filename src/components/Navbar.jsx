e0c623f74a3b5846079b4ed0facbebd52f8af666
Timothy King <king_tim@hotmail.co.uk>
2023-08-23 12:45:09 +0100

    Added a close menu icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Navbar.css';
 
 export default function Navbar() {
@@ -58,7 +58,7 @@ export default function Navbar() {
         <nav className="navigation">
             <div className="nav-button-container">
                 <button className="nav-icon" onClick={handleClick}>
-                    <FontAwesomeIcon icon={faBars} size="2xl" color="#2e99d6" />
+                    <FontAwesomeIcon icon={isNavExpanded ? faTimes : faBars} size="2xl" color="#2e99d6" />
                 </button>
             </div>
             <div className={navMenuClassName}>
