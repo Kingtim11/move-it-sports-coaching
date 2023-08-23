3073a24055396bb3867c4ebcd96c0a9331a37f0b
Timothy King <king_tim@hotmail.co.uk>
2023-08-23 12:26:01 +0100

    Screen size event listener and scroll lock

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,63 +6,74 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Navbar.css';
 
 export default function Navbar() {
-
     const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+    function NavMenuItem({ to, children }) {
+        return (
+            <li>
+                <Link to={to} onClick={handleClick}>
+                    {children}
+                </Link>
+            </li>
+        );
+    }
 
     useEffect(() => {
         function handleClickOutside(event) {
             if (!event.target.closest('.nav-menu') && !event.target.closest('.nav-icon')) {
                 document.body.setAttribute('id', 'nav-menu');
-                //document.body.style.overflowY = 'visible';
                 setIsNavExpanded(false);
             }
         }
-        document.addEventListener('click', handleClickOutside)
-        return () => {
-            document.removeEventListener('click', handleClickOutside)
+
+        document.addEventListener('click', handleClickOutside);
+
+        // Add event listener for screen width and navbar expansion
+        function handleScreenWidth() {
+            if (window.innerWidth < 1200 && isNavExpanded) {
+                document.body.style.overflowY = 'hidden';
+            } else {
+                document.body.style.overflowY = 'visible';
+            }
         }
-    }, []);
-    
+
+        // Call the handleScreenWidth initially and whenever isNavExpanded changes
+        handleScreenWidth();
+        window.addEventListener('resize', handleScreenWidth);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+            window.removeEventListener('resize', handleScreenWidth);
+        };
+    }, [isNavExpanded]);
+
     function handleClick() {
         setIsNavExpanded(!isNavExpanded);
-        if (!isNavExpanded) {
-            document.body.setAttribute('id', 'nav-expanded');
-            //document.body.style.overflowY = 'hidden';
-          } else {
-            document.body.setAttribute('id', 'nav-menu');
-            //document.body.style.overflowY = 'visible';
-          }
+        document.body.setAttribute('id', isNavExpanded ? 'nav-menu' : 'nav-expanded');
     }
 
     const navMenuClassName = isNavExpanded ? 'nav-menu expanded' : 'nav-menu';
-    
+
     return (
-        
-            <nav className='navigation'>   
-                <div className='nav-button-container'>
-                    <button className='nav-icon' onClick={handleClick}>
-                        <FontAwesomeIcon  icon={faBars} size='2xl' color='#2e99d6' />
-                    </button>
-                </div>
-                    
-                    <div className={navMenuClassName}>
-                    <ul>
-                        <li className="current"><Link to="/" onClick={handleClick}>Home</Link></li>
-                        <li><Link to="/what-we-offer" onClick={handleClick}>What we Offer</Link></li>
-                        <li><Link to="/meet-the-team" onClick={handleClick}>Meet the team</Link></li>
-                        <li><Link to="/bookings" onClick={handleClick}>Bookings</Link></li>
-                        <li>
-                            <HashLink 
-                                smooth to="/#contact" 
-                                scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })} 
-                                onClick={handleClick}
-                                >
-                                Contact
-                            </HashLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        
+        <nav className="navigation">
+            <div className="nav-button-container">
+                <button className="nav-icon" onClick={handleClick}>
+                    <FontAwesomeIcon icon={faBars} size="2xl" color="#2e99d6" />
+                </button>
+            </div>
+            <div className={navMenuClassName}>
+                <ul>
+                    <NavMenuItem to="/" onClick={handleClick}>Home</NavMenuItem>
+                    <NavMenuItem to="/what-we-offer" onClick={handleClick}>What we Offer</NavMenuItem>
+                    <NavMenuItem to="/meet-the-team" onClick={handleClick}>Meet the team</NavMenuItem>
+                    <NavMenuItem to="/bookings" onClick={handleClick}>Bookings</NavMenuItem>
+                    <li>
+                        <HashLink smooth to="/#contact" scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })} onClick={handleClick}>
+                            Contact
+                        </HashLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
