eb6650c55de074f0a7485960dac6a2799cc2248a
Timothy King <king_tim@hotmail.co.uk>
2023-08-23 14:27:15 +0100

    Refactoring

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -9,7 +9,6 @@ export default function Bookings() {
         src="https://joshua-ayres-sports-coaching.class4kids.co.uk"
         width="100%"
         height="860px"
-        loading="lazy"
         title="A booking form for Joshua Ayres Sports Coaching"
       ></iframe>
     </div>
--- src/components/PreLoad.jsx
+++ src/components/PreLoad.jsx
@@ -7,21 +7,9 @@ import Neil from "../assets/images/coachNeil.jpg";
 import Hollie from "../assets/images/coachHollie.jpeg";
 import Robert from "../assets/images/coachRobert.jpg";
 
-//const imagesToPreload = [{image: Logo}, {image: Banner}, {image: Josh}, {image: Abi}, {image: Neil}, {image: Hollie}, {image: Robert}];
-
 export default function PreLoad() {
-    /*useEffect(() => {
-        imagesToPreload.forEach(image => {
-            const img = new Image();
-            img.src = image.image;
-        });
-    }, []);
-
-    return null; */
     const cardImage = [Logo, Banner, Josh, Abi, Neil, Hollie, Robert]
 
-    /*const cardImage = [
-        {image: Logo}, {image: Banner}, {image: Josh}, {image: Abi}, {image: Neil}, {image: Hollie}, {image: Robert}]*/
     return (
         cardImage.map((cardImage, index) => (
             <React.Fragment key={index}>
