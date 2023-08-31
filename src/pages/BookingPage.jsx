c56a1d41c77e850b27e2af4b047ca02208a2eab3
Timothy King <king_tim@hotmail.co.uk>
2023-08-31 13:05:11 +0100

    Footer removed from all pages apart from HomePage

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,24 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Bookings from "../components/Bookings";
 import Sections from "../components/Sections";
  
 export default function BookingPage() {
-    function FooterSection() {
-        return (
-            <div id="copyright">
-                <ul>
-                    <li>© 2023 Move it! Sports Coaching Ltd. All rights reserved.</li>
-                    <li>Design: <Link className='link-style2' to="http://html5up.net">HTML5 UP</Link></li>
-                    <li>Web Development: <Link className='link-style2' to="https://timking.netlify.app/">Tim King</Link></li>
-                </ul>
-                <ul>
-                    <li>Move it! Sports Coaching Ltd is a company registered in England and Wales with Company Number 14588755</li>
-                </ul>
-            </div>
-        )
-    }
-
     const sections = [
         {
             sectionId: 'header',
@@ -29,10 +13,6 @@ export default function BookingPage() {
             divClassName: 'title',
             sectionHeader: 'Bookings',
             content: <Bookings />
-        },
-        {
-            sectionId: 'footer',
-            content: <FooterSection />
         }
     ];
 
